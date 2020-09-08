31f30ef4f767136cc1433d52cebe6dccf91f0d69
MattSmithies <info@mattsmithies.co.uk>
2020-09-08 10:44:30 +0100

    Basic models

---
+++ src/Models/HashgraphConsensusMessage.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Trustenterprises\LaravelHashgraph\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class HashgraphConsensusMessage extends Model
+{
+    protected $guarded = [];
+}
---
+++ src/Models/HashgraphTopic.php
@@ -0,0 +1,12 @@
+<?php
+
+
+namespace Trustenterprises\LaravelHashgraph\Models;
+
+
+use Illuminate\Database\Eloquent\Model;
+
+class HashgraphTopic extends Model
+{
+    protected $guarded = [];
+}
